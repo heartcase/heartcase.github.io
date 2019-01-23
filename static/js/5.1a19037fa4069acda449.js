1e6e3eb86ab193e3c97ca24a4e31b82c9f6ed11f
liboyang <lby920101@gmail.com>
2019-01-23 13:21:33 +0800

    simple blog

---
+++ static/js/5.1a19037fa4069acda449.js
@@ -0,0 +1,2 @@
+webpackJsonp([5],{"914x":function(e,n){e.exports={title:"My second post",content:"Hello, my friends!"}}});
+//# sourceMappingURL=5.1a19037fa4069acda449.js.map
---
+++ static/js/6.7b0c7418ac0e54111491.js
@@ -0,0 +1,2 @@
+webpackJsonp([6],{"c/wd":function(t,e){t.exports={title:"My very first post",content:"Hello, my friends!"}}});
+//# sourceMappingURL=6.7b0c7418ac0e54111491.js.map
